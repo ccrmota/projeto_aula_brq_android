c01c26b8414aeaeb8c389762adeac94f47e106e5
ccrmota <ccrismota222@gmail.com>
2021-05-04 15:20:03 -0300

    Meu commit orientação a objeto

---
+++ app/src/main/java/br/cicero/orientobjeto/PrimeiraActivity.kt
@@ -0,0 +1,11 @@
+package br.cicero.orientobjeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrimeiraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primeira)
+    }
+}
